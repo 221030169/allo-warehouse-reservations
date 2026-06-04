8ba562a766936776bf74883f9578333b82713904
221030169 <priyanshlunawat2207@gmail.com>
2026-06-04 13:08:50 +0530

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
+  migrations: {
+    // This tells Prisma to execute your seed file using ts-node
+    seed: 'npx ts-node prisma/seed.ts',
   },
 });
